955a3ac63e3859c9555603a7370a8c65d2cd3b5e
= <=>
2023-03-22 22:40:36 +0100

    yes

--- src/main.c
+++ src/main.c
@@ -2,17 +2,6 @@
 #include <eleve.h>
 int main(void)
 {
-	eleve el;
-	unsigned char nom[LEN];
-
-
 	
-	
-	scanf("%s",nom);
-	setEleveNom(&el,nom);
-	scanf("%s",nom);
-	setElevePrenom(&el,nom);
-	printf("%s\n", el.nom);
-	printf("%s\n", el.prenom);
 	return 0;
 }
